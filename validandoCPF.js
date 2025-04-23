9ba8e00c594dd9c0271d26e79ed6966ed6d2d8b5
Pedro Amaral <127465620+PPedroAmaral@users.noreply.github.com>
2025-04-23 17:26:53 -0300

    class validadorCPF.js

--- validandoCPF.js
+++ validandoCPF.js
@@ -1,57 +1,61 @@
-let cpf = '123.456.789-10'; //insira um CPF
-
-function limparCPF(cpf) {
-    return cpf.replace(/\D+/g, '');
-}
-
-let cpfLimpo = limparCPF(cpf);
-
-function converterCPFParaArrayDeNumeros(cpfLimpo) {
-    return Array.from(cpfLimpo).map(Number);
-}
-
-let valores = converterCPFParaArrayDeNumeros(cpfLimpo);
-let multiplicadores = [10, 9, 8, 7, 6, 5, 4, 3, 2];
-let multiplicadores2 = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2];
-
-function calcularDigitoVerificador(numeros, multiplicadores) {
-    return numeros.map((numero, index) => numero * multiplicadores[index]);
-}
-
-let resultado1 = calcularDigitoVerificador(valores.slice(0, 9), multiplicadores);
-let resultado2 = calcularDigitoVerificador(valores.slice(0, 10), multiplicadores2);
-
-function somaArray (array) {
-    return array.reduce((ac, valor) => {
-        return ac + valor;
-    },0)
-}
-
-let soma1 = somaArray(resultado1)
-let soma2 = somaArray(resultado2)
-
-function primerioDigito (soma) {
-    let digito = 11 - (soma % 11);
-    if(digito > 9){
-        return 0
-    } else {
-        return digito
+class CPFValidador {
+    constructor(cpf) {
+        this.cpf = cpf;
+        this.cpfLimpo = this.limparCPF();
+        this.valores = this.converterCPFParaArray();
     }
+
+    limparCPF() {
+        return this.cpf.replace(/\D+/g, '');
+    }
+
+    converterCPFParaArray(){
+        return Array.from(this.cpfLimpo).map(Number); 
+    }
+
+    calcularDigitoVerificador(numeros, multiplicadores) {
+        return numeros.map((numero, index) => numero * multiplicadores[index]);
+    }
+
+    somaArray(array) {
+        return array.reduce((acumulador, valor) => acumulador + valor, 0);
+    }
+
+    primeiroDigito(soma) {
+        const digito = 11 - (soma % 11);
+        return digito > 9 ? 0 : digito;
+        
+    }
+
+    validarCPF() {
+        if (this.cpfLimpo.length !== 11) {
+            return "CPF Inválido!";
+        }
+
+        const multiplicadores1 = [10, 9, 8, 7, 6, 5, 4, 3, 2];
+        const multiplicadores2 = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2];
+
+        const resultado1 = this.calcularDigitoVerificador(this.valores.slice(0,9),multiplicadores1);
+        const soma1 = this.somaArray(resultado1);
+        const digito1 = this.primeiroDigito(soma1);
+
+        const resultado2 = this.calcularDigitoVerificador(this.valores.slice(0,10),multiplicadores2);
+        const soma2 = this.somaArray(resultado2);
+        const digito2 = this.primeiroDigito(soma2); 
+
+        const penultimoNumero = this.valores[this.valores.length - 2];
+        const ultimoNumero = this.valores[this.valores.length - 1];
+        
+        if (penultimoNumero === digito1 && ultimoNumero === digito2) {
+            return "CPF Válido!";
+          } else {
+            return "CPF Inválido!";
+          }
+    }
+
 }
 
-let digito1 = primerioDigito(soma1);
-let digito2 = primerioDigito(soma2);
-
-function verificadorCPF(valores, digito1, digito2){
-    const ultimoNumero = valores[valores.length -1];
-    const penultimoNumero = valores[valores.length -2];
-
-    if (penultimoNumero === digito1 && ultimoNumero === digito2){
-        return "CPF Válido!"
-    }else {
-        return "CPF Inválido!";
-      }
-}
-
-let cpfVerificado = verificadorCPF(valores, digito1, digito2);
-console.log(cpfVerificado);
+let cpf = '123.456.789-10';  // insira o cpf
+const validador = new CPFValidador(cpf);
+const resultadoVadidacao = validador.validarCPF();
+console.log(resultadoVadidacao);
